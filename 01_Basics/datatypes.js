5c0c5358d010f3438d7be2e0b0a65c052d7bf466
Rajender Reddy B <79013189+Pekka1721@users.noreply.github.com>
2024-05-06 10:02:43 +0000

    Datatypes

---
+++ 01_Basics/datatypes.js
@@ -0,0 +1,15 @@
+//use strict to treat all js code as newer version
+//Datatypes
+//primitives
+/**
+ * Number - range(2**53)
+ * bigInt - 
+ * String - ""
+ * Boolean- true/false
+ * Null - Standalone value/type (type is object)
+ * Undefined - type is undefined
+ * Symbol - Unique
+ */
+
+//NOn premitive
+//objects
--- 01_Basics/test.js
+++ 01_Basics/test.js
@@ -1,1 +1,12 @@
-console.log("Codespaces Env");
+const accoundId = 14455324
+let accountEmail = "something@gmail.com";
+var accountPassword="12345";
+
+accountCity = "Jaipur"
+//accoundId = "kslhgoan" // re assigning to a cont variable is not allowed
+
+let accountState;
+accountEmail = "xcxc@xc+gmail.com";
+accountPassword="1223456789";
+accountCity="Hyderabad";
+console.table([accoundId,accountEmail,accountPassword,accountState,accountCity])
