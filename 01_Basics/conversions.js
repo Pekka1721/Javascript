8419bbf4149abdc188c73ac54a2874eaa7f4efa7
Rajender Reddy B <79013189+Pekka1721@users.noreply.github.com>
2024-05-06 11:33:32 +0000

    conversions in js

---
+++ 01_Basics/conversions.js
@@ -0,0 +1,15 @@
+let str1 = "Hello";
+let str2 = " Username";
+
+let str3 = str1+str2;
+//console.log(str3);
+
+// console.log('1'+2); //12 converts to string
+// console.log(1+"2"); //12  converts to string
+
+// console.log('1'+2+2); //122
+// console.log(1+2+"3"); //33
+
+
+console.log(+true); //not to use (but it converts to number)
+//prefix 
